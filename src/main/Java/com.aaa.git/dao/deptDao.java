b77f8ca14c10a5f2a274e0b83de89ef70cbd8b8c
githubzpz0121 <1507077781@qq.com>
2019-04-19 09:15:22 +0800

    第4次 提交dao  修改的是deptDao 输出的东西

---
+++ src/main/Java/com.aaa.git/dao/deptDao.java
@@ -0,0 +1,14 @@
+package com.aaa.git.dao;
+
+/**
+ * fileName:deptDao
+ * description:
+ * author:zpz
+ * createTime:2019/4/19 9:14
+ */
+public class deptDao {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+    }
+
+}
